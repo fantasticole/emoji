062d08223b26a7fef4563b82df53a6b4a6179436
cole <coleamurphy@gmail.com>
2020-07-02 19:19:35 -0400

    Add copy button to Selecton This copies the entire group as a string. Also updated styles so the Selection bar scrolls when there are a lot of emoji selected.

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 class Selection extends React.Component {
+  copySelected = () => {
+    const chars = this.props.selected.map(({character}) => character);
+    const emojiString = chars.join("");
+    navigator.clipboard.writeText(emojiString);
+  }
+
   render() {
     return (
       <div className="selection">
-        {this.props.selected.map(({character, slug, unicodeName}, i) => (
-          <button title={`delete ${unicodeName} from selected group`}
-                  className="emoji"
-                  key={slug}
-                  onClick={() => this.props.removeEmoji(i)}>
-            {character}
-          </button>
-        ))}
+        <button className="copy" onClick={this.copySelected}>copy</button>
+        <div className="group">
+          {this.props.selected.map(({character, slug, unicodeName}, i) => (
+            <button title={`delete ${unicodeName} from selected group`}
+                    className="emoji"
+                    key={i}
+                    onClick={() => this.props.removeEmoji(i)}>
+              {character}
+            </button>
+          ))}
+        </div>
       </div>
     );
 
