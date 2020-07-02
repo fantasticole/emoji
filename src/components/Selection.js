2e2afaa231876ec953b1ea61b6f2db306ece5dee
cole <coleamurphy@gmail.com>
2020-07-02 18:40:03 -0400

    Create Selection component to list selected emoji

---
+++ src/components/Selection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Selection extends React.Component {
+  render() {
+    return (
+      <div className="selection">
+        {this.props.selected.map(({character, slug, unicodeName}, i) => (
+          <button title={unicodeName}
+                  className="selected"
+                  key={slug}
+                  onClick={() => this.props.removeEmoji(i)}>
+            {character}
+          </button>
+        ))}
+      </div>
+    );
+
+  }
+}
+
+export default Selection;
