493e4df70438e2801ea4607f3667c8a9585368c9
cole <coleamurphy@gmail.com>
2020-07-02 17:07:18 -0400

    Grab emojis from api rathher than a static file

--- src/App.js
+++ src/App.js
@@ -4,19 +4,38 @@ import './styles/index.css';
 
 import EmojiList from './components/EmojiList';
 
-import emoji from './utils/emoji';
+import {EMOJI_API_KEY} from './utils/key';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      emojiList: [],
+      error: "",
+    };
+  }
+
+  componentDidMount() {
+    const listUrl = `https://emoji-api.com/emojis?access_key=${EMOJI_API_KEY}`;
+
+    fetch(listUrl)
+      .then(res => res.json())
+      .then(
+        (emojiList) => {
+          this.setState({emojiList});
+        },
+        (error) => {
+          this.setState({error});
+        }
+      )
   }
 
   render() {
     return (
       <div className="app">
-        <EmojiList emoji={emoji} />
+        <h1>Emoji List</h1>
+        <EmojiList emoji={this.state.emojiList} />
       </div>
     );
   }
